f3b5d0218af35f9adc389867936226a01c81f031
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2020-03-20 17:02:57 +1300

    Some work on the start script. Gets the list of components from the graph rather than hard-coding

---
+++ parse_components.rb
@@ -0,0 +1,21 @@
+# Parse thge specificed JSON file and get the list of processes
+# Scan the processes to get the component name
+# Append .coffee to the component name and change the folder name from msgflo-components to components
+# msgflo-components is the name in the msgflo world, components is the folder on disk
+
+# Derek Knight
+# March 2020
+
+require "json"
+
+file = File.read(ARGV[0])
+data = JSON.parse(file)
+
+processes = data['processes']
+
+processes.each do | key, value |
+  component = value['component']
+  implementation = "#{component}.coffee".sub! 'msgflo-components', 'components'
+
+  puts "./node_modules/.bin/msgflo-nodejs --name #{key} #{implementation}"
+end
